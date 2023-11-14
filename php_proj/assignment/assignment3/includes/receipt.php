54c7c9e6f79c5ead737b44bd1bf5e8341b130c13
charmjo <charmjohannes.relator@gmail.com>
2023-11-14 11:30:37 -0500

    changed php shorthand tags to echo to cover more compatability

--- php_proj/assignment/assignment3/includes/receipt.php
+++ php_proj/assignment/assignment3/includes/receipt.php
@@ -1,10 +1,75 @@
-<?php
-// anyways, output is global this should work.
-    if(isset($output["key"])) {
-        echo "subtotal is less than 10";
-    } else {
-         echo "YOU SHALL PASS!";
-    }
-
-
-?>
+<!-- 
+- I wanted to minimize the number of PHP tags as possible so it is easier to read. 
+- The thing is, I prefer the presence of php tags rather than echoing html tags inside php. I find it harder to read html tags in strings.
+- This little template will show the output.
+- personal notes: do this last coz I know you have the most experience when it comes to things like this.
+-->
+<div class="output-wrapper">
+    <!-- if subtotal is lesser than 10 -->
+    <?php
+        if(isset($output["key"])) {
+    ?>
+    <!-- insert html here -->
+    <div class="no-receipt">
+        <h3>Your receipt cannot be displayed because the total cost of items you bought is less than 10. We apologize.</h3>
+    </div>
+    <!-- if subtotal is more than 10 -->
+    <?php
+        } else {
+            $personInfo = $output["person_info"];
+            // CHARM DO NOT FORGET TO PUT THE DOLLARS coz it caused you your grade. okay?
+    ?>
+    <!-- insert html here -->
+    <section class="user-info">
+        <div class="person-info">
+            <ul>
+                <li>Sold to: <?php echo $personInfo["uname"]; ?></li>
+                <li>Email: <?php echo $personInfo["uemail"]; ?></li>
+                <li>Phone: <?php echo $personInfo["uphone"]; ?></li>
+                <li>Address: <?php echo "{$personInfo["uaddress"]}, {$personInfo["ucity"]}, {$personInfo["uprovince"]}, {$personInfo["upostcode"]}";?></li>  
+            </ul>
+        </div>
+        <div class="credit-info">
+            <ul>
+                <li>Number: <?php echo $personInfo["ucred_num"];?></li>
+                <li>Expiry: <?php echo "{$personInfo["ucred_month"]}/{$personInfo["ucred_year"]}"; ?></li>
+            </ul>
+        </div>
+    </section>
+    <table class="product-info">
+        <tr>
+            <th>Product Name</th>
+            <th>Unit Price</th>
+            <th>Quantity</th>
+            <th>Total</th>
+        </tr>
+        <!-- insert php for loop here for tables -->
+        <?php
+        $productInfo = $output["product_info"];
+        $products = $productInfo["products"];
+        foreach ($products as $row) {
+        ?>
+            <tr>
+                <td><?php echo $row["name"];?></td>
+                <td>$<?php echo $row["unit_price"];?></td>
+                <td><?php echo $row["qty"];?></td>
+                <td>$<?php echo $row["price"];?></td>
+            </tr>
+        <?php
+        }
+        ?>
+        <!-- end of for loop tables -->
+    </table>
+    <section class="total">
+    <!-- try to align this on the right -->
+        <ul>
+            <li>Subtotal: $<?php echo $productInfo["subtotal"];?></li>
+            <li>Sales Tax: (<?php echo $productInfo["tax_rate"];?>%): $<?php echo $productInfo["sales_tax"];?></li>
+            <li>Grand Total: $<?php echo $productInfo["total"];?></li>
+        </ul>
+    </section>
+    <!-- end tag for else -->
+    <?php 
+        }
+    ?>
+</div>
--- php_proj/assignment/assignment3/index.php
+++ php_proj/assignment/assignment3/index.php
@@ -1,68 +1,65 @@
-<?php require('includes/config.php'); 
-      require('process.php');
-      print_r($_POST);
-?>
-<?php ?>
+<!-- Notes
+ - I tried researching on how to do {{$variable}} in html. Apparently, to implement this, a tempating engine like blade or twig have to be used.
+ - Another way is to make a feature for this but that would be overkill.
+-->
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="assets/css/style.css">
     <title>Document</title>
 </head>
 <body>
+    <?php 
+        require('includes/config.php'); 
+        require('process.php');
+    ?>
     <?php include('includes/header.php'); ?>
     <main>
         <form method="post" action="">
             <div class="form-container">
-                <div class="form-wrapper">
+                <section class="form-wrapper pad-20-all">
+                    <h1>Order Form</h1>
                     <div class="field-wrapper">
                         <div class="labels">
                             <label for="uname">Name</label>
-                            <span id="nameError"></span>
                         </div>    
-                        <input value="<?php echo $person["uname"]; ?>" type="text" name="uname" id="uname" placeholder="John Doe"> 
+                        <input value="<?php echo $person["uname"];?>" type="text" name="uname" id="uname" placeholder="John Doe"> 
                     </div>
-                    <div class="fieldWrapper">
+                    <div class="field-wrapper">
                         <div class="labels">
                             <label for="uemail">Email</label>
-                            <span id="emailError"></span>
                         </div>
                         <input value="<?php echo $person["uemail"];?>" type="text" name="uemail" id="uemail" placeholder="johndoe@test.com"> 
                     </div>
-                    <div class="fieldWrapper">
+                    <div class="field-wrapper">
                         <div class="labels">
                             <label for="uphone">Phone</label>
-                            <span id="phoneError"></span>
                         </div>
                         <input value="<?php echo $person["uphone"];?>" type="text" name="uphone" id="uphone" placeholder="123-123-1234"> 
                     </div>
-                    <div class="fieldWrapper">
+                    <div class="field-wrapper">
                         <div class="labels">
                             <label for="upostcode">Post Code</label>
-                            <span id="postCodeError"></span>
                         </div>
                         <input value="<?php echo $person["upostcode"];?>" type="text" name="upostcode" id="upostcode" placeholder="A1B2C3"> 
                     </div>
-                    <div class="fieldWrapper">
+                    <div class="field-wrapper">
                         <div class="labels">
                             <label for="uaddress">Address</label>
-                            <span id="addressError"></span>
                         </div>
                         <input value="<?php echo $person["uaddress"];?>" type="text" name="uaddress" id="uaddress" placeholder="101 Redfox Grove">             
                     </div>
-                    <div class="fieldWrapper">
+                    <div class="field-wrapper">
                         <div class="labels">
                             <label for="ucity">City</label>
-                            <span id="cityError"></span>
                         </div>
                         <input value="<?php echo $person["ucity"];?>" type="text" name="ucity" id="ucity" placeholder="Waterloo">
                     </div>
-                    <div class="fieldWrapper">
+                    <div class="field-wrapper">
                         <div class="labels">
                             <label for="uprovinces">Province</label>
-                            <span id="provincesError"></span>
                         </div> 
                         <select name="uprovinces" id="uprovinces">
                             <option value="">-- SELECT PROVINCE --</option> 
@@ -84,29 +81,25 @@
                     
                     <div class="creditCardSection">
                         <hr>
-                        <h2 class="pad-20-all"> Credit Card Details</h2>
-                        <div class="fieldWrapper">
+                        <h3> Credit Card Details</h3>
+                        <div class="field-wrapper">
                             <div class="labels">
                                 <label for="ucred_num">Credit Card Number</label>
-                                <span id="creditCardNumError"></span>
                             </div>
-                            
                             <input value="<?php echo $person["ucred_num"];?>" type="text" name="ucred_num" id="ucred_num" placeholder="4111-1111-1111-1111">
                         </div>
             
-                        <div class="fieldWrapper">
+                        <div class="pad-20-all">
                             <h4>Credit Card Expiry</h4>
-                                <div class="fieldWrapper">
+                                <div class="field-wrapper">
                                     <div class="labels">
-                                        <label for="ucreditExpiryMonth">Month:</label>
-                                        <span id="creditExpiryMonthError"></span>
+                                        <label for="ucred_month">Month:</label>
                                     </div>
                                     <input value="<?php echo $person["ucred_month"];?>" type="text" name="ucred_month" id="ucred_month" placeholder="JAN" minlength="3" maxlength="3">
                                 </div>
-                                <div class="fieldWrapper">
+                                <div class="field-wrapper">
                                     <div class="labels">
-                                        <label for="ucreditExpiryYear">Year:</label>
-                                        <span id="creditExpiryYearError"></span>
+                                        <label for="ucred_year">Year:</label>
                                     </div>
                                     <input value="<?php echo $person["ucred_year"];?>" type="text" name="ucred_year" id="ucred_year" placeholder="2010">
                                 </div>              
@@ -114,35 +107,57 @@
                         <hr>
                     </div>
 
-                    <div class="fieldWrapper"> 
+                    <div class="field-wrapper"> 
                         <div class="labels">
                             <label for="upassword">Password</label>
-                            <span id="passwordError"></span>
                         </div>    
                         <input value="<?php echo $person["upassword"];?>" type="password" name="upassword" id="upassword"> 
                     </div>
-                    <div class="fieldWrapper"> 
+                    <div class="field-wrapper"> 
                         <div class="labels">
-                            <label for="uconfirmPassword">Confirm Password</label>
-                            <span id="confirmPasswordError"></span>
+                            <label for="uconfirm_password">Confirm Password</label>
                         </div>    
                         <input value="<?php echo $person["uconfirm_password"];?>" type="password" name="uconfirm_password" id="uconfirm_password"> 
                     </div>
                     
-                </div>
-                <div class="product-wrapper">
-
-                </div>
-            </div>    
-
-            <?php if ($errors) include('includes/error-messages.php');?>
+                </section>
+                <section class="product-wrapper pad-20-all">
+                    <h1>Product List</h1>
+                    <ul>
+                        <li class="pad-20-all">
+                            <img src="assets/images/ffxiv_cookbook.JPG" alt="The Ultimate Final Fantasy XIV Cookbook by Victoria Rosenthal">
+                            <div class="productInfo pad-20-sides">
+                                <h3 class="productTitle">The Ultimate Final Fantasy XIV Cookbook</h3>
+                                <p class="productsubtitle">by Victoria Rosenthal</p>
+                                <h3 class="price">$2.25</h3>
+                                <div class="qtySelect">
+                                    <label for="cookbook_qty" hidden> Cookbook Qty</label>
+                                    <input class="qtyField" type="number" name="cookbook_qty" id="cookbook_qty" min="0" max="999" value="0" placeholder="0">
+                                </div>
+                            </div>
+                        </li>
             
+                        <li class="pad-20-all">
+                            <img src="assets/images/saad_book.JPG" alt="Systems Analysis and Design in a Changing World">
+                            <div class="productInfo pad-20-sides">
+                                <h3 class="productTitle">Systems Analysis and Design in a Changing World</h3>
+                                <p class="productsubtitle">by John Satzinger</p>
+                                <h3 class="price">$3.75</h3>
+                                <div class="qtySelect">
+                                    <label for="saadbook_qty" hidden> Cookbook Qty</label>
+                                    <input class="qtyField" type="number" name="saadbook_qty" id="saadbook_qty" min="0" max="999" value="0" placeholder="0">
+                                </div>
+                            </div>
+                        </li>
+                    </ul>
+                </section>
+            </div>    
             <div class="submitWrapper pad-20-all">
                 <input type="submit" value="Submit">
             </div>
         </form>
-
-        <?php if ($errors) include('includes/receipt.php'); ?>
+        <?php if ($errors) include('includes/error-messages.php');?>
+        <?php if ($output) include('includes/receipt.php'); ?>
 
     </main>
     <?php require('includes/footer.php'); ?>
